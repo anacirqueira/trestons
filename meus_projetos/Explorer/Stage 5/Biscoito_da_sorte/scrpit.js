8f6d6db254138d36f500292c61f949d641becfe6
anacirqueira <cirqueiraana6@gmail.com>
2023-07-26 16:38:46 -0300

    Funções

--- meus_projetos/Explorer/Stage 5/Biscoito_da_sorte/scrpit.js
+++ meus_projetos/Explorer/Stage 5/Biscoito_da_sorte/scrpit.js
@@ -1,17 +1,35 @@
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const btnTentar = document.querySelector("#btnTentar")
-const btnReset = document.querySelector("#btnReset")
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTentar = document.querySelector('#btnTentar')
+const btnReset = document.querySelector('#btnReset')
+const msg = document.querySelector('.text p')
 
-function handleTryClick(event) {
-  screen1.classList.add("hide")
-  screen2.classList.remove("hide")
+const frase = [
+  'Alguns biscoitos da sorte não contêm nenhuma sorte',
+  'Abençoadas são as crianças pois elas vão herdar o débito nacional',
+  'Não seja um c#zão',
+  'Pegue outro biscoito da sorte',
+  'Pare de enrolar - comece amanhã',
+  'A maior de todas as torres começa no solo',
+  'Todas as coisas são difíceis antes de se tornarem fáceis'
+];
+
+const randomFrase = frase[Math.round(Math.random() * frase.length)];
+
+function toggleSreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
 }
 
-function handleReset() {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handleTryClick() {
+  toggleSreen()
+  msg.innerText = `${randomFrase}`
+}
+
+function handleNew() {
+  toggleSreen()
+  randomFrase = frase[Math.round(Math.random() * frase.length)];
 }
 
 btnTentar.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', handleReset)
+btnReset.addEventListener('click', handleNew)
