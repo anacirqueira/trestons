c17ea1343db5a93d90eff22e48d6c9bc47cd8a98
anacirqueira <cirqueiraana6@gmail.com>
2023-07-27 14:34:53 -0300

    Ajuste no nome das variáveis

--- meus_projetos/Explorer/Stage 5/Biscoito_da_sorte/scrpit.js
+++ meus_projetos/Explorer/Stage 5/Biscoito_da_sorte/scrpit.js
@@ -1,9 +1,3 @@
-const screen1 = document.querySelector('.screen1')
-const screen2 = document.querySelector('.screen2')
-const btnTentar = document.querySelector('#btnTentar')
-const btnReset = document.querySelector('#btnReset')
-const msg = document.querySelector('.text p')
-
 const frase = [
   'Alguns biscoitos da sorte não contêm nenhuma sorte',
   'Abençoadas são as crianças pois elas vão herdar o débito nacional',
@@ -11,25 +5,32 @@ const frase = [
   'Pegue outro biscoito da sorte',
   'Pare de enrolar - comece amanhã',
   'A maior de todas as torres começa no solo',
-  'Todas as coisas são difíceis antes de se tornarem fáceis'
+  'Todas as coisas são difíceis antes de se tornarem fáceis',
 ];
 
-const randomFrase = frase[Math.round(Math.random() * frase.length)];
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const abrirBiscoito = document.querySelector('#abrirBiscoito')
+const novoBiscoito = document.querySelector('#novoBiscoito')
+const msg = document.querySelector('.text p')
 
-function toggleSreen() {
+let randomFrase = frase[Math.round(Math.random() * frase.length)];
+
+function toggleScreen() {
   screen1.classList.toggle('hide')
   screen2.classList.toggle('hide')
 }
 
-function handleTryClick() {
-  toggleSreen()
+const handleTryClick = () => {
+  toggleScreen()
   msg.innerText = `${randomFrase}`
 }
 
 function handleNew() {
-  toggleSreen()
+  toggleScreen()
+  location.reload()
   randomFrase = frase[Math.round(Math.random() * frase.length)];
 }
 
-btnTentar.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', handleNew)
+abrirBiscoito.addEventListener('click', handleTryClick);
+novoBiscoito.addEventListener('click', handleNew);
