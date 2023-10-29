7e95b52ad50ac2c5754343023cb4841ff4057c5a
anacirqueira <cirqueiraana6@gmail.com>
2023-10-29 16:41:04 -0300

    First commit

---
+++ Desktop/trestons/js/index.js
@@ -0,0 +1,15 @@
+/*
+
+import { Router } from './router.js'
+
+const router = new Router()
+router.add('/', 'pages/home.html')
+router.add('/quemsomos', 'pages/quemsomos.html')
+router.add('/servicos', 'pages/servicos.html')
+router.add('/contato', 'pages/contato.html')
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
+*/
