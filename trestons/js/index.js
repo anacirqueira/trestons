b4ae2569637178b996d61b9cce935236bf6f4567
Ana Cirqueira <71336555+anacirqueira@users.noreply.github.com>
2023-10-30 11:01:48 -0300

    scroll com JavaScript

--- trestons/js/index.js
+++ trestons/js/index.js
@@ -1,5 +1,4 @@
 /*
-
 import { Router } from './router.js'
 
 const router = new Router()
@@ -13,3 +12,21 @@ router.handle()
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
 */
+
+const menuItens = document.querySelectorAll('nav a')
+
+menuItens.forEach(item => {
+  item.addEventListener('click', scroll);
+})
+
+function scroll(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute('href');
+  const to = document.querySelector(id).offsetTop;
+  
+  window.scroll({
+    top: to - 110,
+    behavior: "smooth",
+  });
+}
